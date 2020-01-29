59ab4eeed131e722339abce84dd7df1b74d8bbd2
Martin Trenker <trenker.martin@gmail.com>
2020-01-29 12:47:13 +0100

    add v4 domain config to static-site
    closes #2

--- packages/cdk-static-site/test/cdk-static-site.test.ts
+++ packages/cdk-static-site/test/cdk-static-site.test.ts
@@ -1,12 +1,39 @@
 import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import { App, Stack } from '@aws-cdk/core';
 import { StaticSite } from '../lib';
+import { HostedZone } from '@aws-cdk/aws-route53';
 
-describe('static site construct', () => {
-  it('creates the S3 Bucket', () => {
+describe("static site construct", () => {
+  it("creates the S3 Bucket", () => {
     const app = new App();
-    const stack = new Stack(app, "TestStack");
-    new StaticSite(stack, 'StaticSiteConstruct');
+    const stack = new Stack(app, "TestStack", {
+      env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+      }
+    });
+    new StaticSite(stack, "StaticSiteConstruct");
     expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+  });
+
+  it("Creates a A record", () => {
+    const app = new App();
+    const stack = new Stack(app, "TestStack", {
+      env: {
+        account: "123456789101112",
+        region: "us-east-1"
+      }
+    });
+    new StaticSite(stack, "StaticSiteConstruct", {
+      domainConfig: {
+        zone: HostedZone.fromLookup(stack, "TestZone", {
+          domainName: "example.com",
+        }),
+        domainName: "foo.example.com"
+      }
+    });
+    expectCDK(stack).to(haveResource("AWS::Route53::RecordSet", {
+      "Type": "A"
+    }));
   });
 })
